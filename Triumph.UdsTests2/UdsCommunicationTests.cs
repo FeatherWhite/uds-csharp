bf7a176a05c705cc29797aea7e5bce8c38a55efe
FeatherWhite <tomw5823@outlook.com>
2025-04-10 18:28:12 +0800

    添加测试

---
+++ Triumph.UdsTests2/UdsCommunicationTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triumph.UdsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triumph.UdsLibrary.Tests
+{
+    [TestClass()]
+    public class UdsCommunicationTests
+    {
+        [TestMethod()]
+        public void DiagnosticReceiveCanFrameTest()
+        {
+            UdsCommunication uds = new UdsCommunication();
+            uds.DiagnosticRequestPid(Uds.DiagnosticPidRequestType.DIAGNOSTIC_ENHANCED_PID, 0x790, new byte[] { 0x00,0x05});
+
+            uds.DiagnosticReceiveCanFrame();
+        }
+    }
+}
